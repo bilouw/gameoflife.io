c670cb79d6841a6ed35757a73759ede6cf7ce5b5
Bilouw <thomas.bardon@outlook.com>
2019-04-01 03:02:51 +0200

    added some features

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -4,12 +4,12 @@
 			<h1 class="column column-offset-25">Game Of Life Online</h1>
 		</div>
 		<div class="row" style="margin-top: 10px;">
-			<table class="column column-50 column-offset-25" v-if="worldExist" cellspacing="0">
+			<table class="column column-offset-25" v-if="worldExist" cellspacing="0">
 				<thead>
 				</thead>
 				<tbody>
-					<tr v-for="h in height" :key="h.id">
-							<td v-for="w in width" :key="w.id" :style="getCellColor(h-1, w-1)" class="cell" @click="setWorldCell(h-1, w-1)"/>
+					<tr v-for="h in worldSize" :key="h.id">
+							<td v-for="w in worldSize" :key="w.id" :style="getCellStyle(h-1, w-1)" @click="setWorldCell(h-1, w-1)"/>
 					</tr>
 				</tbody>
 			</table>
@@ -17,6 +17,14 @@
 			</div>
 		<div class="row" style="margin-top: 20px;">
 			<button class="column column-10 column-offset-25" @click="randomize()">Randomize</button>
+			<button class="column column-10 column-offset-10" @click="setPlaying()">Play/Pause</button>
+			<button class="column column-10 column-offset-10" @click="cleanWorld()">Clean</button>
+
+		</div>
+		<div class="row" style="margin-top: 20px;">
+			<input class="column column-10 column-offset-25" type="number" placeholder="World Size" v-model.number="worldSize"  @change="setWorldSize(worldSize)">
+			<input class="column column-10 column-offset-10" type="range" min="1" max="20" value="10" v-model.number="cellSize">
+			<input class="column column-10 column-offset-10" type="checkbox" v-model="coloredCell">
 		</div>
 	</div>
 </template>
@@ -34,8 +42,9 @@ export default {
 	},
 	data:() => {
 		return {
-			width: 50,
-			height: 50,
+			worldSize: 50,
+			cellSize: 10,
+			coloredCell: true,
 			gen: 0,
 			score: 0,
 			world: null,
@@ -52,15 +61,12 @@ export default {
 	},
 	computed: {
 		worldExist() {
-			let ret = true;
-			if (this.world == null) ret = false;
-			return ret;
+			return this.world == null ? false: true;
 		}
 	},
 	methods: {
-		getCellColor(h, w) {
+		getCellStyle(h, w) {
 			let color = '#000';
-			//console.log(this.world[h][w])
 			if (this.world[h][w]['alive']) {
 				switch(this.world[h][w]['color']) {
 					case 0: color = '#C4BBAF'; break;
@@ -68,8 +74,10 @@ export default {
 					case 2: color = '#248232'; break;
 					case 3: color = '#57B8FF'; break;
 				}
+				if (!this.coloredCell) color = '#fff';
 			}
-			return 'background-color: ' + color;
+			let size = '; width : ' + this.cellSize + 'px; ' + 'height: ' + this.cellSize + 'px;';
+			return 'background-color: ' + color + size;
 		},
 		update() {
 			if(this.play) {
@@ -79,7 +87,7 @@ export default {
 		},
 		init() {
 			// instantiate a Kuzzle client
-			this.$kuzzle = new Kuzzle(new WebSocket('163.172.174.150')); // 192.168.0.25 for accessing from outside
+			this.$kuzzle = new Kuzzle(new WebSocket('localhost')); // 192.168.0.25 for accessing from outside
 
 			// add a listener to detect any connection problems
 			this.$kuzzle.on('networkError', error => {
@@ -127,6 +135,10 @@ export default {
 				 action: 'updateWorld'
 			 };
 
+			this.request(query);
+		},
+
+		async request(query) {
 			try {
          const response = await this.$kuzzle.query(query, {});
 				 console.log(response._result);
@@ -143,13 +155,7 @@ export default {
 				 y: y
 			 };
 
-			 try {
-         const response = await this.$kuzzle.query(query, {});
-				 console.log(response._result);
-       } catch (error) {
-				 console.log(error);
-       }
-
+			 this.request(query);
 		},
 
 		async randomize() {
@@ -158,16 +164,41 @@ export default {
          action: 'randomizeWorld'
 			 };
 			 
-			 try {
-         const response = await this.$kuzzle.query(query, {});
-				 console.log(response._result);
-       } catch (error) {
-				 console.log(error);
-       }
+			 this.request(query);
 		},
+
+		async setPlaying() {
+			const query = {
+         controller: 'kuzzle-core-plugin-gameoflife/worldController',
+         action: 'setPlaying'
+			 };
+			 
+			 this.request(query);
+		},
+
+		async cleanWorld() {
+			const query = {
+         controller: 'kuzzle-core-plugin-gameoflife/worldController',
+         action: 'cleanWorld'
+			 };
+			 
+			 this.request(query);
+		},
+
+		async setWorldSize(size) {
+			const query = {
+         controller: 'kuzzle-core-plugin-gameoflife/worldController',
+				 action: 'setWorldSize',
+				 size: size
+			 };
+
+			 this.request(query);
+		},
+
 		destroyed: function () {
 			clearInterval(this.$timer)
 		}	
+
 	}
 }
 </script>
